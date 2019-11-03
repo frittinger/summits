af42a9975872b310f80511473932c43c9b129b2e
Frank Rittinger <frank.rittinger@virtual-identity.com>
2019-11-03 22:59:24 +0100

    fix: Change JsonSerializer from Jackson to Kafka
    With the Jackson Json serializer, sending of custom messages failed
    on initialization. Changing to the Kafka serializer fixed this behavior.

--- user-service/java/src/main/java/com/virtualidentity/summits/userservice/KafkaProducerConfig.java
+++ user-service/java/src/main/java/com/virtualidentity/summits/userservice/KafkaProducerConfig.java
@@ -1,6 +1,5 @@
 package com.virtualidentity.summits.userservice;
 
-import com.fasterxml.jackson.databind.JsonSerializer;
 import com.virtualidentity.summits.userservice.model.User;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
@@ -10,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
